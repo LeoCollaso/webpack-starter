bccbf8b2ec2180cbc726778d5bd33bea197068b3
Leobardo <leo.collaso@gmail.com>
2022-07-26 14:08:44 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import { saludar } from './js/componentes' // No importa si no se especifica la extensión porque busca arvhivos .js
+import './styles.css';
+
+const nombre = 'Leo';
+
+saludar( nombre );
